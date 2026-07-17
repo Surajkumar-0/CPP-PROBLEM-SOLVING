454c11141ed1d00aee799b45159b15f0c0bb52bc
Suraj Kumar <surajkushwaha2659@gmail.com>
2026-07-17 08:56:08 +0530

    Sum of Two Numbers program.

---
+++ 02_problem.cpp
@@ -0,0 +1,11 @@
+// Write a C++ program to input two numbers and print their sum.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a , b;
+    cout<<"Enter the Number : ";
+    cin>>a>>b;
+
+    cout<<"The sum of two Number is : "<<a+b;
+}
