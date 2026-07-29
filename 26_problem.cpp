19aff5b48621edd25089536ab866ec15ccbc48ea
Suraj Kumar <surajkushwaha2659@gmail.com>
2026-07-29 22:58:35 +0530

    Factorial program.

---
+++ 26_problem.cpp
@@ -0,0 +1,19 @@
+// Write a C++ program to find the factorial of a number.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long long fact = 1;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    cout << "Factorial: " << fact << endl;
+    return 0;
+}
