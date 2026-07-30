c5e667d7f9f259956254a2d0c9d4811cb40251d3
Suraj Kumar <surajkushwaha2659@gmail.com>
2026-07-30 23:45:55 +0530

    reverse number program.

---
+++ 27_problem.cpp
@@ -0,0 +1,22 @@
+// Write a C++ program to reverse a given number.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num, reversedNum = 0, remainder;
+
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    while (num != 0) {
+        remainder = num % 10;                 
+        reversedNum = reversedNum * 10 + remainder; 
+        num /= 10;                             
+    }
+
+    cout << "Reversed Number = " << reversedNum << endl;
+
+    return 0;
+}
